158325626517e9610170e1a797afec2043dd90f0
Josecarlos Vidal <Josecarlosvidal2@gmail.com>
2026-02-10 15:10:32 -0300

    Eliminar archivo de desarrollo

--- main.mjs
+++
@@ -1,4 +0,0 @@
-import { latexToAst, latexToMathml, DisplayStyle } from "./index.js";
-
-console.log(latexToAst(String.raw`\frac{1}{2}`))
-console.log(latexToMathml(String.raw`\frac{1}{2}`, DisplayStyle.Block))
